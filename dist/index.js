aff7a8758d3c79386564e764bc91cc22cfecec36
Alexander Duffaut <aduffaut@ufl.edu>
2022-01-07 21:38:43 -0500

    completed mikro-orm setup and migrator to postgres

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const constants_1 = require("./constants");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = async () => {
-    const orm = await core_1.MikroORM.init({
-        entities: [],
-        dbName: 'lireddit',
-        type: 'postgresql',
-        debug: !constants_1.__prod__
-    });
+    const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
+    await orm.getMigrator().up();
 };
-console.log("Hello There");
+main();
 //# sourceMappingURL=index.js.map
